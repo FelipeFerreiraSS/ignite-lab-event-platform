4b7723f11d42c40216b518a932763d51f37d1e20
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-06-21 15:03:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import  { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id 
+      title
+
+      teacher {
+        name
+      }
+    }
+  }
+`
+
+interface Lessons {
+  id: string;
+  title: string;
+}
 
 function App() {
+  const { data } = useQuery<{ lessons: Lessons[] }>(GET_LESSONS_QUERY) 
+  
   return (
-    <h1 className="text-5xl font-bold text-blue-600">Hello World!!!</h1>
+    <ul>
+      {data?.lessons.map(lesson => {
+        return (
+          <div key={lesson.id}>
+            <li>{lesson.title}</li>
+          </div>
+        )
+      })}
+    </ul>
   )
 }
 
